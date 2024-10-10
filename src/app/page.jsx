e855b68e3b3faa9a64482ca2d82e0f1e0dcbdb83
vijay4527 <vijaysaroj4527@gmail.com>
2024-10-10 20:02:18 +0530

    new changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./page.module.css";
-import Articles from "@/components/Articles/Articles";
+import Articles from "@/components/Articles/articles";
 export default function Home() {
   return (
     <>
