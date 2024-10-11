38244d3baeab71c4ad5e0f018739c09be12db582
ADA-AbdusSamad <ada.abdussamad@gmail.com>
2024-10-11 18:09:01 +0530

    about us page

---
+++ src/app/about/page.js
@@ -0,0 +1,22 @@
+import Navbar from '@/components/Navbar/Navbar'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <div style={{
+            backgroundImage: 'url("about-banner.jpg")',
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            height: "80vh", // Full viewport height
+            width: "100%", // Full width
+            position: "relative",
+          }}>
+
+
+          </div>
+    </div>
+  )
+}
+
+export default page
