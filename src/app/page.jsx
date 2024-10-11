2984f13a34bf4709465a47934bc4bb26e70cdcc8
vijay4527 <vijaysaroj4527@gmail.com>
2024-10-11 15:50:17 +0530

    new changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,12 +15,26 @@ export default function Home() {
             position: "relative",
           }}
         >
-          <div className="container h-100">
+          {/* Transparent black overlay */}
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: "rgba(0, 0, 0, 0.5)", // Black with 50% opacity
+              zIndex: 1, // Ensure it appears above the background image
+            }}
+          ></div>
+
+          {/* Content */}
+          <div
+            className="container h-100"
+            style={{ position: "relative", zIndex: 2 }}
+          >
             <div className="row align-items-center justify-content-center text-center h-100">
               <div className="col-md-7">
-                {/* <h2 className="heading mb-5" style={{ color: "#fff" }}>
-                Free Website Template for Charity Websites.
-              </h2> */}
                 <p style={{ display: "inline-block" }}>
                   <a
                     href="https://vimeo.com/channels/staffpicks/93951774"
@@ -31,7 +45,7 @@ export default function Home() {
                     <span className="play-icon-wrap align-self-center mr-4">
                       <span className="ion-ios-play"></span>
                     </span>
-                    <span className="align-self-center">Watch Video</span>
+                    {/* <span className="align-self-center">Watch Video</span> */}
                   </a>
                 </p>
               </div>
@@ -39,7 +53,70 @@ export default function Home() {
           </div>
         </div>
       </div>
+
       <Articles />
+      <div
+        className="featured-section overlay-color-2 impactStories"
+        style={{
+          backgroundImage:
+            "url('https://themewagon.github.io/givehope/images/bg_3.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          position: "relative",
+        }}
+      >
+        {/* Transparent color overlay */}
+        <div
+          className="overlay"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(247, 202, 68, 0.8)", // #f7ca44 with 60% transparency
+            zIndex: 1,
+          }}
+        ></div>
+
+        <div className="container" style={{ position: "relative", zIndex: 2 }}>
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src="https://themewagon.github.io/givehope/images/bg_3.jpg"
+                alt="Image placeholder"
+                className="img-fluid"
+              />
+            </div>
+
+            <div className="col-md-6 pl-md-5">
+              <span className="featured-text d-block mb-3">
+                Success Stories
+              </span>
+              <h2>
+                Water Is Life. We Successfully Provide Clean Water in South East
+                Asia
+              </h2>
+              <p className="mb-3">
+                Far far away, behind the word mountains, far from the countries
+                Vokalia and Consonantia, there live the blind texts.
+              </p>
+              <span className="fund-raised d-block mb-5">
+                We have raised $100,000
+              </span>
+
+              <p>
+                <a
+                  href="#"
+                  className="btn btn-success btn-hover-white py-3 px-5"
+                >
+                  Read The Full Story
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,71 @@
-import React from 'react'
-import styles from "@/components/Navbar/Navbar.module.css"
+import React from 'react';
+import styles from "@/components/Navbar/Navbar.module.css";
+
 const Navbar = () => {
-    
   return (
-    <nav className={`navbar navbar-expand-lg ${styles["navbar-dark"]} ${styles["ftco_navbar"]} ${styles["bg-dark"]} ${styles["ftco-navbar-light"]}`} id="ftco-navbar">
-    <div className={`container ${styles.navContainer}`}>
-      <a className={styles["navbar-brand"]} href="index.html">GiveHope</a>
-      <button className={styles["navbar-toggler"]} type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="oi oi-menu"></span> Menu
-      </button>
+    <nav
+      className={`navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light`}
+      id="ftco-navbar"
+    >
+      <div className={`container ${styles.navContainer}`}>
+        {/* <a className={styles["navbar-brand"]} href="/">
+          GiveHope
+        </a> */}
+        <img src="logo.png" alt="" style={{width:" 3.8rem"}}/>
+        {/* <button
+          className={styles["navbar-toggler"]}
+          type="button"
+          data-toggle="collapse"
+          data-target="#ftco-nav"
+          aria-controls="ftco-nav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="oi oi-menu"></span> Menu
+        </button> */}
 
-      <div className="collapse navbar-collapse" id="ftco-nav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item active"><a href="index.html" className="nav-link">Home</a></li>
-          <li className="nav-item"><a href="how-it-works.html" className="nav-link">How It Works</a></li>
-          <li className="nav-item"><a href="donate.html" className="nav-link">Donate</a></li>
-          <li className="nav-item"><a href="gallery.html" className="nav-link">Gallery</a></li>
-          <li className="nav-item"><a href="blog.html" className="nav-link">Blog</a></li>
-          <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
-          <li className="nav-item"><a href="contact.html" className="nav-link">Contact</a></li>
-        </ul>
+        <div className="collapse navbar-collapse" id="ftco-nav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/how-it-works" className="nav-link">
+                How It Works
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/donate" className="nav-link">
+                Donate
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/gallery" className="nav-link">
+                Gallery
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/blog" className="nav-link">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/about" className="nav-link">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/contact" className="nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-  )
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
